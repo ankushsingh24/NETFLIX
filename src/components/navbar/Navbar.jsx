9764caac12b3c5ed816635b30f05a6bc85118f55
Ankush Singh <ankushsahil45@gmail.com>
2022-05-24 02:16:22 +0530

    fixed the navbar at the top

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
   return (
     <div className="navbar">
       <div className="container">
@@ -10,20 +11,24 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png"
             alt=""
           />
+
           <span>HomePage</span>
           <span>Series</span>
           <span>Movies</span>
           <span>New and Popular</span>
           <span>My List</span>
         </div>
+
         <div className="right">
           <Search className="icon" />
-          <span>Search</span>
+          <span>KID</span>
           <Notifications className="icon" />
+
           <img
             src="https://images.pexels.com/photos/6899268/pexels-photo-6899268.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
+
           <div className="profile">
             <ArrowDropDown className="icon" />
             <div className="options">
@@ -37,4 +42,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar; // 18:30
+export default Navbar;
